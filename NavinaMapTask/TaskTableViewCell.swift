70bd5f1fadba3f22e48197bbf3659e0573e0952a
NavinaM <navinaviji.cs@gmail.com>
2025-01-23 10:13:19 +0530

    add card view on list view

--- NavinaMapTask/TaskTableViewCell.swift
+++ NavinaMapTask/TaskTableViewCell.swift
@@ -8,13 +8,22 @@
 import UIKit
 
 class TaskTableViewCell: UITableViewCell {
+    @IBOutlet weak var mainView: UIView!
+
     @IBOutlet weak var title: UILabel!
     @IBOutlet weak var desc: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
+        cardview()
         // Initialization code
     }
-
+    func cardview(){
+        mainView.layer.cornerRadius = 20.0
+        mainView.layer.shadowColor = UIColor.gray.cgColor
+        mainView.layer.shadowOffset = CGSize(width: 0.0, height: 0.0)
+        mainView.layer.shadowRadius = 12.0
+        mainView.layer.shadowOpacity = 0.7
+    }
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
